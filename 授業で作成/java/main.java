d963cc6829155512d25c2aba5872050a70a89d1d
Niwa0506 <85533653+Niwa0506@users.noreply.github.com>
2023-04-02 21:18:39 +0900

    Add files via upload

---
+++ 授業で作成/java/main.java
@@ -0,0 +1,11 @@
+class main {
+	public static void main(String argv[]) {
+		private String id;
+		Student st = new Student();
+		st.setld("01110");
+		st.setName("Suzu");
+		st.setGrade(100);
+		id=st.getld();
+		System.out.println("ID :"+id);
+	}
+}
